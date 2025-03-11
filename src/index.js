f121879c63b1f1256f666852800941b7f1abf64c
HeoKhoDai <baokhanh20802@gmail.com>
2025-03-11 21:21:44 +0700

    connecting

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-import store from "./Store";
+import store from "./store";
+import { Provider } from "react-redux";
 
 store.dispatch({ type: "account/deposit", payload: 250 });
 console.log(store.getState());
@@ -11,6 +12,8 @@ console.log(store.getState());
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
